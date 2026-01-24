76b2470180bd904196b7fb49b66572e4ae9d6afc
ruiiu <ruiliu@umd.edu>
2026-01-24 00:23:53 -0500

    rebuttal

--- examples/7b_mvsr_entropy_loss.sh
+++ examples/7b_mvsr_entropy_loss.sh
@@ -52,9 +52,9 @@ nohup python3 -m verl.trainer.main \
     algorithm.spo_run_initialization=true \
     algorithm.text_kl_enabled=false \
     algorithm.use_entropy_loss=true \
-    algorithm.entropy_coef=0.05 \
+    algorithm.entropy_coef=0.1 \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.experiment_name=7b_mvsr_entropy_loss_0.05 \
+    trainer.experiment_name=7b_mvsr_entropy_loss_0.1 \
     trainer.n_gpus_per_node=$RAY_GPU_COUNT \
     > /dev/null 2>&1 &
     # trainer.load_checkpoint_path=../mssr_ckpts/mm-spo/7b_mvsr_entropy_loss_0.05/global_step_65/actor/huggingface
